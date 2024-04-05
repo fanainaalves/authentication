73b77efc6b9a314522e24dfdf5eaae52409bcbcb
Fanaina Alves <fanaiina.allves@gmail.com>
2024-04-05 05:21:22 -0300

    criando o projeto

---
+++ msauthentication/src/test/java/com/edu/msauthentication/MsauthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.msauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsauthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
