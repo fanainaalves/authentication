ba4a9ecb3bea7768df2a76cb719a9adfa5beb6eb
Fanaina Alves <fanaiina.allves@gmail.com>
2024-04-05 05:40:08 -0300

    Criação do AuthController

---
+++ msauthentication/src/main/java/com/edu/msauthentication/auth/controllers/AuthController.java
@@ -0,0 +1,28 @@
+package com.edu.msauthentication.auth.controllers;
+
+import com.edu.msauthentication.auth.services.AuthorizationService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("auth")
+public class AuthController {
+
+    @Autowired
+    AuthorizationService authorizationService;
+
+    @PostMapping("/api/vi/authentication/token/")
+    public ResponseEntity<Object> login(@RequestBody @Valid AuthenticationDTO authenticationDTO){
+        return  authorizationService.login(authenticationDTO);
+    }
+
+    @PostMapping("/api/vi/authentication/validation/")
+    public ResponseEntity<Object> register(@RequestBody RegisterDTO registerDTO){
+        return authorizationService.register(registerDTO);
+    }
+}
